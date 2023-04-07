88a2959b98ad4ca62ced5ef21d8143ed66b39f81
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-04-07 14:00:23 +0330

    Refactor:     Moved to infrastructure/utils

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -8,9 +8,9 @@ import 'application/plain/states/state.plain.dart';
 import 'infrastructure/repositories/repository.audio.dart';
 import 'infrastructure/repositories/repository.configurations.dart';
 import 'infrastructure/repositories/repository.settings.dart';
-import 'infrastructure/routes/route_names.dart';
-import 'infrastructure/routes/routes.dart';
-import 'infrastructure/theme/app.theme.dart';
+import 'infrastructure/utils/routes/app.route_names.dart';
+import 'infrastructure/utils/routes/app.routes.dart';
+import 'infrastructure/utils/theme/app.theme.dart';
 import 'presentation/splash/screen.splash.dart';
 
 void main() async {
