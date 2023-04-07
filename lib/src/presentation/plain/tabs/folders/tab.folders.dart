019ff91bd9c795fa5d2d3b57862da31e445bd88a
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-04-07 12:27:38 +0330

    using AudioLibraryCubit to handle Audio Library converted to stateful widget to have more control over widget rebuild

--- lib/src/presentation/plain/tabs/folders/tab.folders.dart
+++ lib/src/presentation/plain/tabs/folders/tab.folders.dart
@@ -1,131 +1,203 @@
-import 'package:audiotagger/audiotagger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../application/folders/cubit.folders.dart';
-import '../../../../application/folders/state.folders.dart';
+import '../../../../application/audio_library/cubit.audio_library.dart';
+import '../../../../application/audio_library/state/state.audio_library.dart';
+import '../../../../application/plain/bloc.plain.dart';
 import '../../../../domain/audio_library/model.folder.dart';
-import '../../../../infrastructure/repositories/repository.audio.dart';
 import '../../../../infrastructure/utils/app.utils.dart';
+import '../../../shared/widgets/widget.loading.dart';
 import '../../../shared/widgets/widget.scrolling_text.dart';
-import '../plain.tab.i.dart';
+import '../i.plain.tab.dart';
 
-class FoldersTab extends StatelessWidget implements PlainTab {
-  FoldersTab({super.key});
-
-  final Set<String> genreSet = {};
-  final Audiotagger audiotagger = Audiotagger();
+class FoldersTab extends StatefulWidget implements PlainTab {
+  const FoldersTab({super.key});
 
   @override
-  Widget build(final BuildContext context) => BlocProvider(
-        create: (final _) => FoldersCubit(
-          audioRepository: RepositoryProvider.of<AudioRepository>(context),
-        ),
-        child: BlocBuilder<FoldersCubit, FoldersState>(
-          builder: (final context, final state) => FutureBuilder<List<Folder>>(
-            future: BlocProvider.of<FoldersCubit>(context).getFolders(),
-            initialData: const [],
-            builder: (final context, final foldersListSnapshot) =>
-                ListView.builder(
-              shrinkWrap: true,
-              itemBuilder: (final context, final index) {
-                final TextStyle? textStyle =
-                    Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground,
-                        );
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: AppUtils.size_4Pt,
-                    vertical: AppUtils.size_4Pt,
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(
-                        const ContinuousRectangleBorder(),
+  State<FoldersTab> createState() => _FoldersTabState();
+  @override
+  FloatingActionButton? floatingActionButton(final BuildContext context) =>
+      BlocProvider.of<PlainBloc>(context)
+              .audioLibraryHandler
+              .state
+              .folders
+              .isEmpty
+          ? FloatingActionButton.extended(
+              onPressed: () async {
+                await BlocProvider.of<PlainBloc>(context)
+                    .audioLibraryHandler
+                    .addFolder()
+                    .then((final value) {
+                  if (value != null) {
+                    BlocProvider.of<PlainBloc>(context)
+                        .audioLibraryHandler
+                        .loadFolders();
+                  }
+                });
+              },
+              label: const Text('Add a Folder'),
+              icon: const Icon(Icons.add),
+            )
+          : FloatingActionButton(
+              onPressed: () async {
+                await BlocProvider.of<PlainBloc>(context)
+                    .audioLibraryHandler
+                    .addFolder()
+                    .then((final value) {
+                  if (value != null) {
+                    BlocProvider.of<PlainBloc>(context)
+                        .audioLibraryHandler
+                        .loadFolders();
+                  }
+                });
+              },
+              child: const Icon(Icons.add),
+            );
+}
+
+class _FoldersTabState extends State<FoldersTab>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(final BuildContext context) {
+    super.build(context);
+    BlocProvider.of<PlainBloc>(context).audioLibraryHandler.loadFolders();
+
+    return BlocBuilder<AudioLibraryCubit, AudioLibraryState>(
+      builder: (final context, final state) {
+        final List<Folder> folders = state.folders;
+        final bool isLoadingFolders = state.isLoadingFolders;
+
+        final TextStyle? textStyle =
+            Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                );
+        return isLoadingFolders
+            ? Center(
+                child: Loading(color: Theme.of(context).colorScheme.primary),
+              )
+            : (folders.isNotEmpty)
+                ? ListView.builder(
+                    shrinkWrap: true,
+                    itemBuilder: (final context, final index) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: AppUtils.size_4Pt,
+                        vertical: AppUtils.size_4Pt,
                       ),
-                      side: MaterialStateProperty.all(
-                        BorderSide(
-                          width: 1,
-                          color: Theme.of(context).colorScheme.tertiary,
-                        ),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: LayoutBuilder(
-                            builder: (final context, final constraints) =>
-                                Column(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(
-                                    AppUtils.size_4Pt,
-                                  ),
-                                  child: ScrollingText(
-                                    text: foldersListSnapshot.data![index].path
-                                        .split('/')
-                                        .last,
-                                    textStyle: textStyle!,
-                                    size: Size(
-                                      constraints.maxWidth,
-                                      constraints.maxHeight,
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(
-                                    AppUtils.size_4Pt,
-                                  ),
-                                  child: ScrollingText(
-                                    text: foldersListSnapshot
-                                        .data![index].directory.absolute.path
-                                        .replaceFirst('/', ''),
-                                    textStyle: textStyle,
-                                    size: Size(
-                                      constraints.maxWidth,
-                                      constraints.maxHeight,
-                                    ),
-                                  ),
-                                ),
-                              ],
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            const ContinuousRectangleBorder(),
+                          ),
+                          side: MaterialStateProperty.all(
+                            BorderSide(
+                              width: 1,
+                              color: Theme.of(context).colorScheme.tertiary,
                             ),
                           ),
                         ),
-                        IconButton(
-                          onPressed: () => BlocProvider.of<FoldersCubit>(
-                            context,
-                          ).deleteFolder(
-                            foldersListSnapshot.data![index],
-                          ),
-                          icon: const Icon(
-                            Icons.delete,
-                            size: AppUtils.size_24Pt,
-                          ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: LayoutBuilder(
+                                builder: (final context, final constraints) =>
+                                    Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(
+                                        AppUtils.size_4Pt,
+                                      ),
+                                      child: ScrollingText(
+                                        text:
+                                            folders[index].path.split('/').last,
+                                        textStyle: textStyle!,
+                                        size: Size(
+                                          constraints.maxWidth,
+                                          constraints.maxHeight,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(
+                                        AppUtils.size_4Pt,
+                                      ),
+                                      child: ScrollingText(
+                                        text: folders[index]
+                                            .directory
+                                            .absolute
+                                            .path
+                                            .replaceFirst('/', ''),
+                                        textStyle: textStyle,
+                                        size: Size(
+                                          constraints.maxWidth,
+                                          constraints.maxHeight,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (final context) => AlertDialog(
+                                    elevation: 1,
+                                    title: const Text('Delete Folder'),
+                                    icon: const Icon(Icons.dangerous),
+                                    content:
+                                        const Text('Are you sure you want to'
+                                            ' delete this folder?'),
+                                    actions: [
+                                      ElevatedButton(
+                                        onPressed: () =>
+                                            BlocProvider.of<PlainBloc>(context)
+                                                .audioLibraryHandler
+                                                .deleteFolder(
+                                                  folders[index],
+                                                )
+                                                .then((final value) {
+                                          if (value) {
+                                            BlocProvider.of<PlainBloc>(context)
+                                                .audioLibraryHandler
+                                                .loadFolders();
+                                          }
+                                          Navigator.of(context).pop();
+                                        }),
+                                        child: const Text('Yes'),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(),
+                                        child: const Text('No'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                              icon: const Icon(
+                                Icons.delete,
+                                size: AppUtils.size_24Pt,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                );
-              },
-              itemCount: foldersListSnapshot.data?.length,
-            ),
-          ),
-        ),
-      );
+                    itemCount: folders.length,
+                  )
+                : const Center(child: Text('Add a Folder'));
+      },
+    );
+  }
 
   @override
-  FloatingActionButton? floatingActionButton(final BuildContext context) =>
-      FloatingActionButton(
-        onPressed: () async {
-          await BlocProvider.of<FoldersCubit>(context).addFolder();
-        },
-        child: const Icon(Icons.add),
-      );
+  bool get wantKeepAlive => true;
 }
 // ElevatedButton(
 // child: const Icon(Icons.add),
