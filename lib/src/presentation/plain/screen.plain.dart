76650c6ec91a38ee3a4c184e849998447e280d93
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-04-08 12:19:37 +0330

    Feature:     FAB dismiss on scroll

--- lib/src/presentation/plain/screen.plain.dart
+++ lib/src/presentation/plain/screen.plain.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 import 'widgets/widget.plain.tab_bar_view.dart';
@@ -17,7 +18,8 @@ class _PlainScreenState extends State<PlainScreen>
   GlobalKey<PlainTabBarState> tabBarKey = GlobalKey<PlainTabBarState>();
   late PlainTabBarView plainTabBarView;
   late PlainTabBar plainTabBar;
-
+  bool _showFAB = true;
+  bool _hasFAB = true;
   @override
   void initState() {
     super.initState();
@@ -27,6 +29,11 @@ class _PlainScreenState extends State<PlainScreen>
       initialIndex: 0,
     )..addListener(() {
         tabBarKey.currentState?.setState(() {});
+        _hasFAB = plainTabBarView
+                .tabViewList(context)[_tabController.index]
+                .floatingActionButton(context) !=
+            null;
+        _showFAB = true;
         setState(() {});
       });
     plainTabBarView = PlainTabBarView(tabController: _tabController);
@@ -43,21 +50,57 @@ class _PlainScreenState extends State<PlainScreen>
   }
 
   @override
-  Widget build(final BuildContext context) =>
-      AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle(
-          statusBarIconBrightness:
-              (Theme.of(context).brightness == Brightness.light)
-                  ? Brightness.dark
-                  : Brightness.light,
-          statusBarBrightness: Theme.of(context).brightness,
-        ),
-        child: Scaffold(
-          bottomNavigationBar: plainTabBar,
-          body: plainTabBarView,
-          floatingActionButton: plainTabBarView
-              .tabViewList(context)[_tabController.index]
-              .floatingActionButton(context),
-        ),
-      );
+  Widget build(final BuildContext context) {
+    const Duration animationDuration = Duration(milliseconds: 300);
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarIconBrightness:
+            (Theme.of(context).brightness == Brightness.light)
+                ? Brightness.dark
+                : Brightness.light,
+        statusBarBrightness: Theme.of(context).brightness,
+      ),
+      child: Scaffold(
+        bottomNavigationBar: plainTabBar,
+        body: _getBody(),
+        floatingActionButton:
+            _getFloatingActionButton(animationDuration, context),
+      ),
+    );
+  }
+
+  AnimatedSlide? _getFloatingActionButton(
+    final Duration animationDuration,
+    final BuildContext context,
+  ) =>
+      _hasFAB
+          ? AnimatedSlide(
+              duration: animationDuration,
+              offset: _showFAB ? Offset.zero : const Offset(0, 0.5),
+              child: AnimatedOpacity(
+                duration: animationDuration,
+                opacity: _showFAB ? 1 : 0,
+                child: plainTabBarView
+                    .tabViewList(context)[_tabController.index]
+                    .floatingActionButton(context),
+              ),
+            )
+          : null;
+
+  Widget _getBody() => _hasFAB
+      ? NotificationListener<UserScrollNotification>(
+          onNotification: (final notification) {
+            final ScrollDirection direction = notification.direction;
+            setState(() {
+              if (direction == ScrollDirection.reverse) {
+                _showFAB = false;
+              } else if (direction == ScrollDirection.forward) {
+                _showFAB = true;
+              }
+            });
+            return true;
+          },
+          child: plainTabBarView,
+        )
+      : plainTabBarView;
 }
