d5ffbbd98d9bcea978ca96eca659795128cafa53
Pooria Askari Moqaddam <pooriaaskarim@gmail.com>
2023-04-11 12:33:07 +0330

    An interface, 'PlainTab' was defined for PlainScreen TabView Widgets

--- lib/src/presentation/plain/tabs/i.plain.tab.dart
+++ lib/src/presentation/plain/tabs/i.plain.tab.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-abstract class PlainTab extends Widget {
-  const PlainTab({super.key});
-  Widget? floatingActionButton(final BuildContext context);
+abstract class PlainTab extends StatefulWidget {
+  const PlainTab({
+    required this.key,
+  }) : super(key: key);
+  @override
+  final GlobalKey<PlainTabState> key;
+
+  @override
+  PlainTabState createState();
+}
+
+abstract class PlainTabState<T extends PlainTab> extends State<T>
+    with AutomaticKeepAliveClientMixin {
+  Widget? get floatingActionButton;
+  Future<bool> get onWillPop;
+  AppBar? get appBar;
 }
